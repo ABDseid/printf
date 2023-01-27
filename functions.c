5d1ee93351d95ca95850ebe8985333ef5351bc58
ABDseid <abddseid@gmail.com>
2023-01-27 01:13:24 -0800

    functions.c

--- functions.c
+++ functions.c
@@ -118,27 +118,27 @@ int print_int(va_list types, char buffer[],
 
 	n = convert_size_number(n, size);
 
-if (n == 0)
-buffer[i--] = '0';
+	if (n == 0)
+		buffer[i--] = '0';
 
-buffer[BUFF_SIZE - 1] = '\0';
-num = (unsigned long int)n;
+	buffer[BUFF_SIZE - 1] = '\0';
+	num = (unsigned long int)n;
 
-if (n < 0)
-{
-num = (unsigned long int)((-1) * n);
-is_negative = 1;
-}
+	if (n < 0)
+	{
+		num = (unsigned long int)((-1) * n);
+		is_negative = 1;
+	}
 
-while (num > 0)
-{
-buffer[i--] = (num % 10) + '0';
-num /= 10;
-}
+	while (num > 0)
+	{
+		buffer[i--] = (num % 10) + '0';
+		num /= 10;
+	}
 
-i++;
+	i++;
 
-return (write_number(is_negative, i, buffer, flags, width, precision, size));
+	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
 
 /************************* PRINT BINARY *************************/
@@ -153,36 +153,36 @@ return (write_number(is_negative, i, buffer, flags, width, precision, size));
  * Return: Numbers of char printed.
  */
 int print_binary(va_list types, char buffer[],
-int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
-unsigned int n, m, i, sum;
-unsigned int a[32];
-int count;
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
 
-UNUSED(buffer);
-UNUSED(flags);
-UNUSED(width);
-UNUSED(precision);
-UNUSED(size);
+	UNUSED(buffer);
+	UNUSED(flags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
 
-n = va_arg(types, unsigned int);
-m = 2147483648; /* (2 ^ 31) */
-a[0] = n / m;
-for (i = 1; i < 32; i++)
-{
-m /= 2;
-a[i] = (n / m) % 2;
-}
-for (i = 0, sum = 0, count = 0; i < 32; i++)
-{
-sum += a[i];
-if (sum || i == 31)
-{
-char z = '0' + a[i];
+	n = va_arg(types, unsigned int);
+	m = 2147483648; /* (2 ^ 31) */
+	a[0] = n / m;
+	for (i = 1; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			char z = '0' + a[i];
 
-write(1, &z, 1);
-count++;
-}
-}
-return (count);
+			write(1, &z, 1);
+			count++;
+		}
+	}
+	return (count);
 }
